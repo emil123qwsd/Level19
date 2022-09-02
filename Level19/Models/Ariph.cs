3a33171150d74141124b089a9a2c06ea9c7928a1
эмиль ахметханов <emil.ah@yandex.ru>
2022-09-02 23:44:35 +0300

    Добавьте файлы проекта.

---
+++ Level19/Models/Ariph.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Level19.Models
+{
+    static class Ariph
+    {
+        public static double Add(double r) => 2*Math.PI*r;
+    }
+}
---
+++ Level19/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,67 @@
+﻿using Level19.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Level19.ViewModels
+{
+    class MainWindowViewModel:INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        void OnPropertyChanged ([CallerMemberName]string PropertyName = null) 
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+        //private double number1;
+        //public double Number1 
+        //{
+        //    get => number1;
+        //    set 
+        //    {
+        //        number1 = value;
+        //        OnPropertyChanged();
+        //    }
+        //}
+        private double number2;
+        public double Number2
+        {
+            get => number2;
+            set
+            {
+                number2 = value;
+                OnPropertyChanged();
+            }
+        }
+        private double number3;
+        public double Number3
+        {
+            get => number3;
+            set
+            {
+                number3 = value;
+                OnPropertyChanged();
+            }
+        }
+        public ICommand AddCommand { get; }
+        private void OnAddCommandExecute(object p) 
+        {
+            Number3 = Ariph.Add(Number2);
+        }
+        private bool CanAddCommandExecuted(object p) 
+        {
+            if (Number2 != 0)
+                return true;
+            else
+                return false;
+        }
+        public MainWindowViewModel() 
+        {
+            AddCommand = new RelayCommand(OnAddCommandExecute, CanAddCommandExecuted);
+        }
+    }
+}
